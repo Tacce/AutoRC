17c3923631fa5252a99db42d9febc0597d44c328
Tacce <102242997+Tacce@users.noreply.github.com>
2024-08-19 19:38:16 +0200

    Now plt plots and stats are saved.

--- statistics.py
+++ statistics.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from sequence import Sequence
 import numpy as np
+import os
 
 sample_step = 2
 num_bins = 10
@@ -10,39 +11,14 @@ framerate = 10
 max_velocity = 2.8
 
 
-def plot_histogram(data, num_bins, title, xlabel):
+def plot_histogram(data, num_bins, title, xlabel, save_path):
     plt.hist(data, bins=num_bins, edgecolor='black')
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel('Frequency')
+    plt.savefig(save_path)
     plt.show()
-
-
-def remove_outliers_length(samples):
-    # Calcola la lunghezza di ogni traiettoria
-    lengths = np.array([sample.get_trajectory_length() for sample in samples])
-
-    # Calcola i quartili e l'IQR
-    Q1 = np.percentile(lengths, 25)
-    Q3 = np.percentile(lengths, 75)
-    IQR = Q3 - Q1
-
-    # Calcola la soglia superiore
-    upper_bound = Q3 + 1.5 * IQR
-    lower_bound = Q1 - 1.5 * IQR
-
-    # Filtra i sample che hanno una traiettoria che supera la soglia superiore
-    filtered_samples = [sample for sample, length in zip(samples, lengths)
-                        if upper_bound >= length >= lower_bound]
-
-    return filtered_samples
-
-
-def remove_outliers_consecutive_distances(samples):
-    filtered_samples = [sample for sample in samples if not sample.is_outlier()]
-    print(f"{len(samples) - len(filtered_samples)} outliers removed")
-    return filtered_samples
-
+    plt.close()
 
 sequences = []
 
@@ -91,51 +67,88 @@ for delta_f in range(2, 9, 2):
                 if curve is not None:
                     stats['curve_angles'].append(curve)
 
+    output_dir = Path(f"dataset_delta_f_{delta_f}")
+    output_dir.mkdir(exist_ok=True)
+
     for trajectory in stats['trajectory_list']:
         plt.plot(trajectory.points[:, 2], -trajectory.points[:, 0])
     plt.title(f'{delta_f} seconds long trajectories')
     plt.axis('equal')
+    plt.savefig(output_dir / f'{delta_f}_seconds_long_trajectories.png')
     plt.show()
+    plt.close()
 
     max_distances = [s.calculate_max_distance_from_origin() for s in samples]
     plot_histogram(max_distances, num_bins, 'Histogram of the Maximum Distances from the Point Cloud to the Origin',
-                   'Maximum Distance')
+                   'Maximum Distance', output_dir / 'histogram_max_distances.png')
 
-    plot_histogram(stats['trajectory_length'], num_bins, 'Histogram of the Trajectory Lengths', 'Trajectory Length')
+    plot_histogram(stats['trajectory_length'], num_bins, 'Histogram of the Trajectory Lengths', 'Trajectory Length',
+                   output_dir / 'histogram_trajectory_lengths.png')
 
     plot_histogram(stats['covered_area'], num_bins, 'Histogram of the Percentage of Area Covered by the Point Cloud',
-                   'Covered Area Percentage')
+                   'Covered Area Percentage', output_dir / 'histogram_covered_area.png')
 
-    plot_histogram(stats['framerates'], num_bins, 'Histogram of the Original Framerates', 'Framerate')
+    plot_histogram(stats['framerates'], num_bins, 'Histogram of the Original Framerates', 'Framerate',
+                   output_dir / 'histogram_framerates.png')
 
-    plot_histogram(stats['distances'], 100, 'Distance Distribution', 'Distance')
+    plot_histogram(stats['distances'], 100, 'Distance Distribution', 'Distance',
+                   output_dir / 'histogram_distances.png')
 
-    plot_histogram(stats['start_angles'], 20, 'Start Angles', 'Start Angles')
+    plot_histogram(stats['start_angles'], 20, 'Start Angles', 'Start Angles',
+                   output_dir / 'histogram_start_angles.png')
 
-    plot_histogram(stats['curve_angles'], 50, 'Curve Angles Distribution', 'Curve Angles')
+    plot_histogram(stats['curve_angles'], 50, 'Curve Angles Distribution', 'Curve Angles',
+                   output_dir / 'histogram_curve_angles.png')
 
-    print(f'{delta_f} SECONDS LONG TRAJECTORIES')
-    print(f"Numero esempi: {stats['n_examples']}")
-    print(f"Numero sequenze a bassa qualità: {stats['low_quality_sequences']}")
-    print(f"Numero sequenze a qualità superiore: {stats['better_quality_sequences']}")
-    print(f"Lunghezza totale sequenze: {np.sum(stats['time_length'])} s")
-    print(f"Lunghezza media sequenze: {np.mean(stats['time_length'])} s")
-    print(f"Lunghezza totale clip a bassa qualità : {np.sum(stats['low_quality_lenght'])} s")
-    print(f"Lunghezza totale clip a qualità superiore: {np.sum(stats['better_quality_lenght'])} s")
-    print(f"Numero medio frame: {np.mean(stats['num_frames'])}")
-    print(f"Numero traitettorie rettilinee: {stats['straight']}")
-    print(f"Numero curve a sinistra: {stats['left']}")
-    print(f"Numero curve a destra: {stats['right']}")
-    print(f"Media lunghezza traiettorie: {np.mean(stats['trajectory_length'])} m")
-    print(f"Varianza lunghezza traiettorie: {np.var(stats['trajectory_length'])}")
-    print(f"Media framerate: {np.mean(stats['framerates'])}")
-    print(f"Varianza framerate: {np.var(stats['framerates'])}")
-    print(f"Media area coperta dalla point-cloud: {np.mean(stats['covered_area'])} %")
-    print(f"Varianza area coperta dalla point-cloud: {np.var(stats['covered_area'])}")
-    print(f"Media distanza punti: {np.mean(stats['distances'])}")
-    print(f"Varianza distanza punti: {np.var(stats['distances'])}")
-    print(f"Media angolo d'attacco: {np.mean(stats['start_angles'])}°")
-    print(f"Varianza angolo d'attacco: {np.var(stats['start_angles'])}")
-    print(f"Media angolo di curva: {np.mean(stats['curve_angles'])}°")
-    print(f"Varianza angolo di curva: {np.var(stats['curve_angles'])}")
-    print("______________________________________________________________________\n")
+    with open(output_dir / 'stats.txt', 'w') as f:
+        print(f'{delta_f} SECONDS LONG TRAJECTORIES')
+        f.write(f'{delta_f} SECONDS LONG TRAJECTORIES\n')
+        print(f"Numero esempi: {stats['n_examples']}")
+        f.write(f"Numero esempi: {stats['n_examples']}\n")
+        print(f"Numero sequenze a bassa qualità: {stats['low_quality_sequences']}")
+        f.write(f"Numero sequenze a bassa qualità: {stats['low_quality_sequences']}\n")
+        print(f"Numero sequenze a qualità superiore: {stats['better_quality_sequences']}")
+        f.write(f"Numero sequenze a qualità superiore: {stats['better_quality_sequences']}\n")
+        print(f"Lunghezza totale sequenze: {np.sum(stats['time_length'])} s")
+        f.write(f"Lunghezza totale sequenze: {np.sum(stats['time_length'])} s\n")
+        print(f"Lunghezza media sequenze: {np.mean(stats['time_length'])} s")
+        f.write(f"Lunghezza media sequenze: {np.mean(stats['time_length'])} s\n")
+        print(f"Lunghezza totale clip a bassa qualità : {np.sum(stats['low_quality_lenght'])} s")
+        f.write(f"Lunghezza totale clip a bassa qualità : {np.sum(stats['low_quality_lenght'])} s\n")
+        print(f"Lunghezza totale clip a qualità superiore: {np.sum(stats['better_quality_lenght'])} s")
+        f.write(f"Lunghezza totale clip a qualità superiore: {np.sum(stats['better_quality_lenght'])} s\n")
+        print(f"Numero medio frame: {np.mean(stats['num_frames'])}")
+        f.write(f"Numero medio frame: {np.mean(stats['num_frames'])}\n")
+        print(f"Numero traitettorie rettilinee: {stats['straight']}")
+        f.write(f"Numero traitettorie rettilinee: {stats['straight']}\n")
+        print(f"Numero curve a sinistra: {stats['left']}")
+        f.write(f"Numero curve a sinistra: {stats['left']}\n")
+        print(f"Numero curve a destra: {stats['right']}")
+        f.write(f"Numero curve a destra: {stats['right']}\n")
+        print(f"Media lunghezza traiettorie: {np.mean(stats['trajectory_length'])} m")
+        f.write(f"Media lunghezza traiettorie: {np.mean(stats['trajectory_length'])} m\n")
+        print(f"Varianza lunghezza traiettorie: {np.var(stats['trajectory_length'])}")
+        f.write(f"Varianza lunghezza traiettorie: {np.var(stats['trajectory_length'])}\n")
+        print(f"Media framerate: {np.mean(stats['framerates'])}")
+        f.write(f"Media framerate: {np.mean(stats['framerates'])}\n")
+        print(f"Varianza framerate: {np.var(stats['framerates'])}")
+        f.write(f"Varianza framerate: {np.var(stats['framerates'])}\n")
+        print(f"Media area coperta dalla point-cloud: {np.mean(stats['covered_area'])} %")
+        f.write(f"Media area coperta dalla point-cloud: {np.mean(stats['covered_area'])} %\n")
+        print(f"Varianza area coperta dalla point-cloud: {np.var(stats['covered_area'])}")
+        f.write(f"Varianza area coperta dalla point-cloud: {np.var(stats['covered_area'])}\n")
+        print(f"Media distanza punti: {np.mean(stats['distances'])}")
+        f.write(f"Media distanza punti: {np.mean(stats['distances'])}\n")
+        print(f"Varianza distanza punti: {np.var(stats['distances'])}")
+        f.write(f"Varianza distanza punti: {np.var(stats['distances'])}\n")
+        print(f"Media angolo d'attacco: {np.mean(stats['start_angles'])}°")
+        f.write(f"Media angolo d'attacco: {np.mean(stats['start_angles'])}°\n")
+        print(f"Varianza angolo d'attacco: {np.var(stats['start_angles'])}")
+        f.write(f"Varianza angolo d'attacco: {np.var(stats['start_angles'])}\n")
+        print(f"Media angolo di curva: {np.mean(stats['curve_angles'])}°")
+        f.write(f"Media angolo di curva: {np.mean(stats['curve_angles'])}°\n")
+        print(f"Varianza angolo di curva: {np.var(stats['curve_angles'])}")
+        f.write(f"Varianza angolo di curva: {np.var(stats['curve_angles'])}\n")
+        print("______________________________________________________________________\n")
+        f.write("______________________________________________________________________\n")
+
